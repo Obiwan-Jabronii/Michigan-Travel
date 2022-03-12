0b19d435136280288fa87d2d3a452a2c47f69339
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-03-12 10:52:47 -0500

    added connection.js and added user-controller

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,36 @@
+const { User} = require('../models')
+
+module.exports = {
+    async getSingleUser({ user = null, params }, res) {
+        const userFound = await User.findOne({
+            $or: [{_id: user ? user._id : params.id}, { username: params.username}],
+        })
+        if(!userFound) {
+            return res.status(400).json({ message:'Cannot find a user with that name '})
+        }
+        res.json(userFound);
+    },
+    
+    async createUser({body}, res) {
+        const user = await User.create(body);
+
+        if(!user) {
+            return res.status(400).json({ message: 'Something went wrong. Please try again.'})
+        }
+        const token = signToken(user);
+        res.json({ token,user }); 
+    },
+
+    async saveLocation({user, body}, res) {
+        try {
+            const updateUser = await User.findOneAndUpdate(
+                {_id: user._id },
+                { $addToSet: {savedLocations: body } },
+                { new: true, runValidators: true }
+            );
+            return res.json(updateUser);
+        } catch (err) {
+            return res.status(400).json(err);
+        }
+    }
+}
