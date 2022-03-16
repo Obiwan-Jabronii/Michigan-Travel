789760cded051303c20d7c19d2d66b79637d7bee
Tyreeze <wedding5@msu.edu>
2022-03-16 10:56:44 -0500

    querires and mutations added

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($commentText: String!) {
+    addComment(commentText: $commentText) {
+      _id
+      commentText
+      createdAt
+      username
+    }
+  }
+`;
+export const UPDATE_USER = gql`
+    mutation updateUser($username: String, $email: String, $password: String) {
+        updateUser(username: $username, email: $email, password: $password) {
+            username
+            email
+            password
+        }
+    }
+`;
+
+export const DELETE_USER = gql`
+    mutation deleteUser($ID: ID!) {
+        deleteUser(_id: $ID) {
+            _id
+            email
+            username
+        }
+    }
+`
