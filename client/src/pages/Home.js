98ca5265df9c1e7a63500979aa9712184b60b2ab
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-09-07 14:30:20 -0400

    added pages for home and signup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import LocationList from '../components/LocatonList';
+import RegionMenu from '../components/RegionMenu';
 
-function HomePage() {
-return (
-     
-    <div className="flex justify-center text-center text-bold text-3xl">
-        <h1>Welcome to Michigan Wanders!</h1>
-        <p className="text-center text-3xl">
-            If you're looking to explore all that the great State of Michigan
-            has to offer, then look no further!
-        </p>
-   
-    <div className='mt-6'>
-    <Link to="/" className=" text-3xl">
-      Click here to explore!
-    </Link>
-    </div>
-    </div>
-    
+const Home = () => {
+    return (
+        <div className='container'>
+            <RegionMenu />
+            <LocationList />
+        </div>
     );
-}
+};
 
-export default HomePage;
-
+export default Home;
